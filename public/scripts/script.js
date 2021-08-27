bc7f92b675c30cc7534405ff7bd80da97f2f6c91
Noorin99 <noorin.sa.99@gmail.com>
2021-08-27 17:53:49 +0300

    creating sign up and log in forms #5

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,18 +1,4 @@
-const signUp = document.querySelector('#Sign-up');
-const signForm = document.querySelector('.sign-up-form');
-const closeSignForm = document.querySelector('.sign-up-close-button');
 
-const popForm = () => {
-    signForm.style.display = "block";
-};
-
-const closeForm = () => {
-    signForm.style.display = "none";
-};
-
-
-signUp.addEventListener('click', popForm);
-closeSignForm.addEventListener('click', closeForm);
 
 
 
